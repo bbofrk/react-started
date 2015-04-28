8f8fb2441169e2c5ef9633a7f2baa06b98b8bf9a
bbofrk <mingliwanglau@gmail.com>
2015-04-28 22:09:17 +1200

    comment

--- server.js
+++ server.js
@@ -1,20 +1,10 @@
 
 //Require our dependencies
 var express= require('express'),
-   exphbs = require('express-handlebars'),
-   http = require('http'),
-   mongoose = require('mongoose'),
-   twitter = require('nttwitter'),
-   routes = require('./routes'),
-   config = require('./config'),
-   streamHandler = require('./utils/streamHandler');
+   http = require('http');
 
 //Create an express instance and set a port varibale
 var app = express();
 var port = process.env.PORT || 8080;
-
-//set handlerbars as the templating engine
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
 
 //Disable etag headers on responses
--- src/helloworld.js
+++ src/helloworld.js
@@ -1,4 +1,52 @@
+var Comment = React.createClass({
+  render: function(){
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+      </div>
+    )
+  }
+});
+
+var CommentList = React.createClass({
+  render: function(){
+    return (
+      <div className="commentList">
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+
+      </div>
+    )
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function(){
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    )
+  }
+});
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
 React.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('example')
-)
+  <CommentBox />,
+  document.getElementById('content')
+);
